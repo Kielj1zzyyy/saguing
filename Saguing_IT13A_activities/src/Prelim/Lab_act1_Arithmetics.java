cfb0915394b4685644bf02c7f6ccaf1f1a5afbdd
balendez_alodia <saguingaaronezekiel@gmail.com>
2025-02-11 13:07:00 +0800

    added helloworld class

---
+++ Saguing_IT13A_activities/src/Prelim/Lab_act1_Arithmetics.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Prelim;
+
+/**
+ *
+ * @author User
+ */
+public class Lab_act1_Arithmetics {
+    public static void main(String[] args){
+    
+    int x = 10;
+    int y = 4;
+    int z = 6;
+    
+    System.out.println(x * y + z);
+    System.out.println(x * y + z);
+    System.out.println(x * y + z);
+    
+            
+   }
+}
