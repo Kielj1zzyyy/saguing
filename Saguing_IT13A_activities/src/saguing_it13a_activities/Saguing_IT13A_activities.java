f6981d6c68d164fd1466f290e618148b240b4c12
balendez_alodia <balendez.alodiazenny26@gmail.com>
2025-02-11 12:48:06 +0800

    Added Java Application

---
+++ Saguing_IT13A_activities/src/saguing_it13a_activities/Saguing_IT13A_activities.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package saguing_it13a_activities;
+
+/**
+ *
+ * @author User
+ */
+public class Saguing_IT13A_activities {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
