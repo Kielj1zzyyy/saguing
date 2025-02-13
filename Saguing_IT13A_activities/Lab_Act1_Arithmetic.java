919f651e5cc4e82668299fd19974195609ee44ae
Aaron Ezekiel M. Saguing <saguingaaronezekiel@gmail.com>
2025-02-13 21:24:20 +0800

    Added a new updated output for my activity1

---
+++ Saguing_IT13A_activities/Lab_Act1_Arithmetic.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Prelim;
+
+/**
+ *
+ * @author sagui
+ */
+public class Lab_Act1_Arithmetic {
+    public static void main(String[] args) {
+      
+        int result1 = 10 * 4 + 6;
+        System.out.println("10*4+6 = " + result1);
+        
+        int result2 = (10 - 4) % 6;
+        System.out.println("(10-4) % 6 = " + result2);
+        
+        int result3 = (10 + 4 + 6) / 3;
+        System.out.println("(10+4+6) / 3 = " + result3);
+        
+        int result4 = 10 * 6 - (4 * 4);
+        System.out.println("10 * 6 - (4 * 4) = " + result4);
+        
+    }
+}
